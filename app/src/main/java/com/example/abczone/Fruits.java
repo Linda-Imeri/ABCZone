c554718bdda7ab1212399b869c7082fc7b1895ca
rinesa muharremi <rinesa.muharremi00@gmail.com>
2021-09-14 00:55:55 +0200

    FruitsActivity design changed.

--- app/src/main/java/com/example/abczone/Fruits.java
+++ app/src/main/java/com/example/abczone/Fruits.java
@@ -20,12 +20,26 @@ public class Fruits extends AppCompatActivity {
         ImageView click5 = (ImageView) findViewById(R.id.orange);
         ImageView click6 = (ImageView) findViewById(R.id.oranges);
 
+        ImageView click7 = (ImageView) findViewById(R.id.carrot);
+        ImageView click8 = (ImageView) findViewById(R.id.carrots);
+        ImageView click9 = (ImageView) findViewById(R.id.tomato);
+        ImageView click10 = (ImageView) findViewById(R.id.tomatos);
+        ImageView click11 = (ImageView) findViewById(R.id.peper);
+        ImageView click12 = (ImageView) findViewById(R.id.peppers);
+
         final MediaPlayer mp1 = MediaPlayer.create(getApplicationContext(), R.raw.apple);
         final MediaPlayer mp2 = MediaPlayer.create(getApplicationContext(), R.raw.apples);
         final MediaPlayer mp3 = MediaPlayer.create(getApplicationContext(), R.raw.banana);
         final MediaPlayer mp4 = MediaPlayer.create(getApplicationContext(), R.raw.bananas);
         final MediaPlayer mp5 = MediaPlayer.create(getApplicationContext(), R.raw.orange);
         final MediaPlayer mp6 = MediaPlayer.create(getApplicationContext(), R.raw.oranges);
+
+        final MediaPlayer mp7 = MediaPlayer.create(getApplicationContext(), R.raw.apple);
+        final MediaPlayer mp8 = MediaPlayer.create(getApplicationContext(), R.raw.apples);
+        final MediaPlayer mp9 = MediaPlayer.create(getApplicationContext(), R.raw.banana);
+        final MediaPlayer mp10 = MediaPlayer.create(getApplicationContext(), R.raw.bananas);
+        final MediaPlayer mp11 = MediaPlayer.create(getApplicationContext(), R.raw.orange);
+        final MediaPlayer mp12 = MediaPlayer.create(getApplicationContext(), R.raw.oranges);
         View.OnClickListener elem = new View.OnClickListener() {
 
             @Override
@@ -49,7 +63,27 @@ public class Fruits extends AppCompatActivity {
                         break;
                     case R.id.oranges:
                         mp6.start();
+                        break;
+
+                    case R.id.carrot:
+                        mp1.start();
+                        break;
+                    case R.id.carrots:
+                        mp2.start();
+                        break;
+                    case R.id.tomato:
+                        mp3.start();
+                        break;
+                    case R.id.tomatos:
+                        mp4.start();
+                        break;
+                    case R.id.peper:
+                        mp5.start();
+                        break;
+                    case R.id.peppers:
+                        mp6.start();
                         break;}
+
             }
         };
         click1.setOnClickListener(elem);
@@ -58,6 +92,12 @@ public class Fruits extends AppCompatActivity {
         click4.setOnClickListener(elem);
         click5.setOnClickListener(elem);
         click6.setOnClickListener(elem);
+        click7.setOnClickListener(elem);
+        click8.setOnClickListener(elem);
+        click9.setOnClickListener(elem);
+        click10.setOnClickListener(elem);
+        click11.setOnClickListener(elem);
+        click12.setOnClickListener(elem);
 
 
 
