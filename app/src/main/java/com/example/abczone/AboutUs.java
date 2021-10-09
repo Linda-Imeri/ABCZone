39199a6bcbf6fec2b5bacc6c3f88401e569356c9
rinesa muharremi <rinesa.muharremi00@gmail.com>
2021-10-10 01:35:39 +0200

    Hyperlinks added at aboutus activity

--- app/src/main/java/com/example/abczone/AboutUs.java
+++ app/src/main/java/com/example/abczone/AboutUs.java
@@ -3,12 +3,17 @@ package com.example.abczone;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
+import android.widget.EditText;
 
 public class AboutUs extends AppCompatActivity {
+EditText edittext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_us);
+        edittext=findViewById(R.id.hyperlinks);
+        edittext.setMovementMethod(LinkMovementMethod.getInstance());
     }
 }
